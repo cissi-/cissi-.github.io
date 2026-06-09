263012d642f4960fe0c60caeb2d6f89f9378be42
Cissi (hemmalaptop) <cissi.wallin@gmail.com>
2026-06-10 01:34:16 +0200

    better tabs and links
     + some cleanup

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,12 @@
 function openTab(evt, tabId) {
   // Declare all variables
-  var i, tabcontent, tablinks;
+  var i, tabcontent, tablinks, tabCategory;
+
+  var element = document.getElementById(tabId);
+  var tabCategory = element.className.replace("tabcontent ", "");
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("tabcontent " + tabCategory);
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
@@ -11,10 +14,12 @@ function openTab(evt, tabId) {
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    if (tablinks[i].parentNode.className.replace("tab ", "") == tabCategory) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabId).style.display = "block";
+  element.style.display = "block";
   evt.currentTarget.className += " active";
 }
